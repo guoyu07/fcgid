d5df0384482222c0bf5ce44327a16abdf9f10d42
Gu Weigang <guweigang@outlook.com>
2013-12-16 19:02:13 +0800

    remove libphnoem

--- thirdparty/libphenom/travis/deps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -x
-case `uname` in
-  Linux)
-    sudo apt-get update -y -q
-    sudo apt-get install -y valgrind -q
-    sudo apt-get install -y libc-ares-dev -q
-    ;;
-esac
--- thirdparty/libphenom/travis/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -x
-./autogen.sh
-./configure --enable-address-sanitizer --with-cares
-make -j
-make -j clang-analyze
-make check
