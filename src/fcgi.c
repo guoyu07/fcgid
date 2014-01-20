d8eb283763c051f77e90c7e497f5deedd91e71b3
ideal <idealities@gmail.com>
2014-01-20 19:02:48 +0800

    change files

--- src/fcgi.c
+++ src/fcgi.c
@@ -1,34 +1,32 @@
-#include "fcgi_header.h"
+/*
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
-#include <unistd.h>
-#include <errno.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <sysexits.h>
+#include "fcgi.h"
+
 #include <stdlib.h>
 
-#include "fcgi_defs.h"
-
-#include "phenom/configuration.h"
-#include "phenom/job.h"
-#include "phenom/log.h"
-#include "phenom/sysutil.h"
-#include "phenom/printf.h"
-#include "phenom/listener.h"
-#include "phenom/socket.h"
-#include "phenom/json.h"
-
 #define BUF_SIZE 5000
-
-// #define FCGI_SERVER "10.48.25.160"
-// #define FCGI_PORT "8888"
-
-// #define FCGI_SERVER "10.48.22.12"
-// #define FCGI_PORT "8990"
-
 
 #define MAXDATASIZE 1000
 
--- src/fcgi_header.h
+++ src/fcgi_header.h
@@ -1,19 +1,39 @@
+/*
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef FCGI_HEADER_H
 #define FCGI_HEADER_H
 
-#include "phenom/defs.h"
-#include "fcgi_defs.h"
-
-#include "phenom/defs.h"
-#include "phenom/configuration.h"
-#include "phenom/job.h"
-#include "phenom/log.h"
-#include "phenom/sysutil.h"
-#include "phenom/printf.h"
-#include "phenom/listener.h"
+// Because phenom/defs.h defined _BSD_SOURCE and _GNU_SOURCE 
+// even if they have been define !
+// To fix compiler warnings we have to put "phenom/socket.h"
+// first...
 #include "phenom/socket.h"
 
-#include <sysexits.h>
+#include "fcgi_defs.h"
+
+#include <stdint.h>
 #include <stdlib.h>
 
 /* Bytes from LSB to MSB 0..3 */
